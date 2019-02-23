2e86b9e9c1813c13c6d1fd9796e9eae1fe7c1df6
zach <zach@example.com>
2019-02-23 21:28:34 +0000

    Added a timestamp

--- flask_app.py
+++ flask_app.py
@@ -56,12 +56,13 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all(), timestamp=datetime.now())
+        return render_template("main_page.html", comments=Comment.query.all())
 
     if not current_user.is_authenticated:
         return redirect(url_for('index'))
