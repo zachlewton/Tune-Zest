c18347b50c8a863fc71b2db8f6d51948858d04c7
zach <zach@example.com>
2019-02-23 19:36:06 +0000

    added login

--- flask_app.py
+++ flask_app.py
@@ -35,7 +35,12 @@ def index():
     db.session.commit()
     return redirect(url_for('index'))
 
-@app.route("/login/")
+@app.route("/login/", methods=["GET", "POST"])
 def login():
-    return render_template("login_page.html")
+    if request.method == "GET":
+        return render_template("login_page.html", error=False)
 
+    if request.form["username"] != "admin" or request.form["password"] != "secret":
+        return render_template("login_page.html", error=True)
+
+    return redirect(url_for('index'))
