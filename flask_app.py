8e0d05e47ec96eee982a83bc1d523dcbc6b5bb94
zach <zach@example.com>
2019-02-23 19:08:20 +0000

    connected database

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,28 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="zestylemon",
+    password="icedog26",
+    hostname="zestylemon.mysql.pythonanywhere-services.com",
+    databasename="zestylemon$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 comments = []
 
 @app.route("/", methods=["GET", "POST"])
